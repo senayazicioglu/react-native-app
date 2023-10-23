71f7ddab10788f8be66b336ac9a48b35470437b4
Sena Betül Yazıcıoğlu <79763384+senayazicioglu@users.noreply.github.com>
2023-10-23 16:05:53 +0300

    Update CounterScreen.js

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -23,3 +23,48 @@ export default function CounterScreen() {
 }
 
 const styles = StyleSheet.create({});
+
+/*USE REDUCE İLE AYNI UYGULAMA*/
+{/*import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
+//state == {count:0}
+//action == {type:"increment" || type:"decrement" , payload}
+
+export default function CounterScreen() {
+  //const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  return (
+    <View>
+      <Button
+        title="Arttır"
+        onPress={() => {
+          dispatch({ type: "increment", payload: 1 });
+          //setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Azalt"
+        onPress={() => {
+          dispatch({ type: "decrement", payload: 1 });
+          //setCounter(counter - 1);
+        }}
+      />
+      <Text>Sayı: {state.count}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+*/}
